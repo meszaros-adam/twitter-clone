0502d170306d350346e9f931124cdf45ad4b657d
meszaros-adam <meszarosadam@outlook.hu>
2023-12-30 19:01:09 +0100

    password change frontend

--- resources/js/pages/settings.vue
+++ resources/js/pages/settings.vue
@@ -18,6 +18,24 @@
                 </div>
                 <button class="button" @click="updateProfile">Update Profile</button>
             </div>
+            <div class="auth-form container">
+                <h1>Password Change</h1>
+                <alertVue :text="alertText"></alertVue>
+                <div class="input-group">
+                    <label for="newPassword">New Password:</label>
+                    <input v-model="newPassword" type="password" id="newPassword" name="newPassword">
+                </div>
+                <div class="input-group">
+                    <label for="newPasswordConfirmation">New Password Confirmation:</label>
+                    <input v-model="newPasswordConfirmation" type="password" id="newPasswordConfirmation"
+                        name="newPasswordConfirmation">
+                </div>
+                <div class="input-group">
+                    <label for="oldPassword">Old Password:</label>
+                    <input v-model="oldPassword" type="password" id="oldPassword" name="oldPassword">
+                </div>
+                <button class="button" @click="updatePassword">Update Password</button>
+            </div>
         </div>
     </div>
 </template>
@@ -38,9 +56,9 @@ export default {
 
 
         const updateProfile = async () => {
-            if (user.value.nickname.trim().length < 4)  return console.log('Nickname must be at least 4 characters!')
-            if (!validateEmail(user.value.email.trim()) ) return console.log('Email must be a valid email!')
-            if (user.value.password.trim().length < 6)  return console.log('Password must be at least 6 characters!')
+            if (user.value.nickname.trim().length < 4) return console.log('Nickname must be at least 4 characters!')
+            if (!validateEmail(user.value.email.trim())) return console.log('Email must be a valid email!')
+            if (user.value.password.trim().length < 6) return console.log('Password must be at least 6 characters!')
             const res = await callApi('post', '/update_user', user.value)
 
             if (res.status == 200) {
@@ -51,7 +69,29 @@ export default {
 
         }
 
-        return { alertText, user, updateProfile }
+        const newPassword = ref('')
+        const newPasswordConfirmation = ref('')
+        const oldPassword = ref('')
+
+        const updatePassword = async () => {
+            if (newPassword.value.trim().length < 6) return console.log('Password must be at least 6 characters!')
+            if (newPassword.value !== newPasswordConfirmation.value) return console.log('Passwords must be at the same!')
+            if (oldPassword.value.trim().length < 6) return console.log('Password must be at least 6 characters!')
+            const res = await callApi('post', '/update_user', {
+                new_password: newPassword.value,
+                new_password_confirmation: newPasswordConfirmation.value,
+                old_password: oldPassword.value
+            })
+
+            if (res.status == 200) {
+
+            }
+            else {
+
+            }
+        }
+
+        return { alertText, user, updateProfile, newPassword, newPasswordConfirmation, oldPassword, updatePassword }
     }
 }
 </script>
